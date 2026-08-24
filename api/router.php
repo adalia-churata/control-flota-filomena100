ae697df34032f4df7f359185470419a2f21a6e46
Adalia <churata123deysi@gmail.com>
2026-08-24 07:53:00 -0500

    solucion del api

--- api/router.php
+++ api/router.php
@@ -443,7 +443,7 @@ if($r0==='maquinaria'){
         $hi=$d['hora_inicio']??null;$hf=$d['hora_fin']??null;
         $th=($hi&&$hf)?round((strtotime($hf)-strtotime($hi))/3600,2):null;
         $act=qone('SELECT factor_carga FROM actividades_retro WHERE id_actividad=?',[$d['id_actividad']]);
-        $ceq=$th!==null&&$act?round($th*(float)$act['factor_carga'],2):null;
+        
         $id=qexec('INSERT INTO retro_control_actividad(id_control_dia,id_actividad,observacion,hora_inicio,hora_fin,total_hora)VALUES(?,?,?,?,?,?)',
             [$d['id_control_dia'],$d['id_actividad'],$d['observacion']??null,$hi,$hf,$th]);
         jout(['id_control_activ'=>(int)$id,'total_hora'=>$th],201);
